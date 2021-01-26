96980fd674e838542eb006a866908c116de55862
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2021-01-26 14:49:34 +0200

    Commit

--- Phonebook Project/App/controllers/phonebook-controller.js
+++ Phonebook Project/App/controllers/phonebook-controller.js
@@ -2,50 +2,57 @@ const Phonebook = require('../models/phonebook-entry');
 
 module.exports = {
     getIndex: function (req, res) {
-        getAll().then((contacts) => {
-            return res.render('index', {contacts});
-        });
+        getAll()
+            .then((contacts) => {
+                return res.render('index', {contacts});
+            });
     },
     getCreate: function (req, res) {
         return res.render('create');
     },
     postCreate: function (req, res) {
         let contactInfo = req.body;
-        addContact(contactInfo).then(() => {
-            return res.redirect('/');
-        })
+        addContact(contactInfo)
+            .then(() => {
+                return res.redirect('/');
+            })
     },
     getEdit: function (req, res) {
         let id = req.params.id;
-        findByID(id).then((contact) => {
-            return res.render('edit', {contact});
-        });
+        findByID(id)
+            .then((contact) => {
+                return res.render('edit', {contact});
+            });
     },
     postEdit: function (req, res) {
         let id = req.params.id;
         let newContactInfo = req.body;
-        editContactInfo(id, newContactInfo).then(() => {
-            return res.redirect('/');
-        });
+        editContactInfo(id, newContactInfo)
+            .then(() => {
+                return res.redirect('/');
+            });
     },
     getDelete: function (req, res) {
         let id = req.params.id;
-        findByID(id).then((contact) => {
-            return res.render('delete', {contact});
-        });
+        findByID(id)
+            .then((contact) => {
+                return res.render('delete', {contact});
+            });
     },
     postDelete: function (req, res) {
         let id = req.params.id;
-        deleteContact(id).then(() => {
-            return res.redirect('/');
-        });
+        deleteContact(id)
+            .then(() => {
+                return res.redirect('/');
+            });
     },
     searchEntries: function(req, res) {
         let searchItem = req.body.searchItem;
-        searchEntries(searchItem).then((result) => {
+        searchEntries(searchItem)
+            .then((result) => {
             // console.log(result);
-            return res.render('index', {result});
-        });
+                return res.render('index', {result});
+            });
     },
 };
 
